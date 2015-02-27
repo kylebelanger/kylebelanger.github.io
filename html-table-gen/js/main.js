82b85427451754e961dc24a27b7cf88b4824c2de
Kyle Belanger <kyle@Kyles-MacBook-Pro.local>
2015-02-27 17:16:26 -0500

    Cleaned up some of the code

--- html-table-gen/js/main.js
+++ html-table-gen/js/main.js
@@ -1,52 +1,52 @@
-/*  This code is kinda a mess, BUT it works correctly...
-    I didn't have enough time to tidy it up yet
+/*  createTable function dynamically creates an HTML table based on
+    user input values. 
+
+    Also generates the multiples of each row based on the number of columns.
+    1 - 10 - 100 - 1000
+    2 - 20 - 200 - 2000
+    3 - 30 - 300 - 3000... etc.
 */
 
 function createTable() {
     
-    // declare variables
+    // dom variables
     var output = document.getElementById('output'),
         table = document.createElement('table'),
         rows = document.getElementById('rows').value,
         columns = document.getElementById('columns').value;
 
-    var multiple = 10;
-    var firstMultiple = 10;
 
-
-
-
-    // Begin creating table
+    //  Begin creating table
+    //  ----------------------------
     // for each row
     for(var r = 0; r < rows; r++) {
 
-        var newNumber = r * 10;
-
+        // loop variables
+        var firstMultiple = 10;             // for first row
+        var newNumber = r * 10;             // all other rows
         var tr = table.insertRow();         // create row
 
         // for each column
         for(var c = 0; c < columns; c++) {
 
-            var td = tr.insertCell();   // create cell
+            // create cell
+            var td = tr.insertCell();
 
-            // if row == 0
+            // if row 0
             if (r == 0) {
-
                 // if first column, then put "N"
                 if (c === 0) {
                     // create column
                     td.appendChild(document.createTextNode('N'));              
                 }
+                // otherwise, put "N" plus the firstMultiple (10, 100, 1000, etc.)
                 else {
                     td.appendChild(document.createTextNode(firstMultiple + ' * N')); 
-                    firstMultiple = firstMultiple * 10;             
-
+                    firstMultiple = firstMultiple * 10;        
                 }
-
-
             }
+            // all other rows
             else {
-
                 // if first column of row
                 if (c === 0) {
                     // create column
@@ -54,15 +54,13 @@ function createTable() {
                 }
                 else {
                     td.appendChild(document.createTextNode(newNumber)); 
-                    newNumber = newNumber * multiple;             
+                    newNumber = newNumber * 10;             
                 }
             }
-
-    
         }
-
-
     }
+    //  ----------------------------
+    //  End creating table
     
     // append table to body
     output.appendChild(table);
